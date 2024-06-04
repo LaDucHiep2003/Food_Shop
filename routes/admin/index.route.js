350ec29cfc76e30a138c048262d918782e3d90a6
LaDucHiep2003 <duchiep045@gmail.com>
2024-06-04 23:11:49 +0700

    role1

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,10 +3,12 @@ const systemConfig = require('../../config/system')
 const dashboardRouters = require('./dashboard.route')
 const productdRouters = require('./product.route')
 const productCategory = require("./product-category.route")
+const roleCategory = require("./role.route")
 
 module.exports = (app) =>{
     const PATH_ADMIN = systemConfig.prefixAdmin
     app.use(PATH_ADMIN + '/dashboard', dashboardRouters)
     app.use(PATH_ADMIN + '/products', productdRouters)
     app.use(PATH_ADMIN + '/products-category',productCategory)
+    app.use(PATH_ADMIN + '/roles', roleCategory)
 }
